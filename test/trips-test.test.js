82ee7126712a5c837a1a9da242fd18a46aa756c8
Michael Walker <Michael.e.walker.87@gmail.com>
2020-09-17 17:51:17 -0600

    Setup base test structure for the trips file

---
+++ test/trips-test.test.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+
+import Trips from '../src/trips';
+import trip from '../src/data/trips-data';
+
+describe('Trips', () => {
+
+  beforeEach(() => {
+
+  });
+
+
+});
