6aba361903d203f3abb32cd8353bc2416e402b04
Michael Walker <Michael.e.walker.87@gmail.com>
2020-09-17 15:52:57 -0600

    Move the travelers data to the right file

--- test/agents-test.js
+++ test/agents-test.js
@@ -1,13 +1,1 @@
-import { expect } from 'chai';
 
-import Travelers from '../src/travelers';
-import travelers from '../src/data/travelers-data';
-
-describe('Travelers', () => {
-
-  beforeEach(() => {
-
-  });
-
-
-});
---
+++ test/travelers-test.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+
+import Travelers from '../src/travelers';
+import travelers from '../src/data/travelers-data';
+
+describe('Travelers', () => {
+
+  beforeEach(() => {
+
+  });
+
+
+});
