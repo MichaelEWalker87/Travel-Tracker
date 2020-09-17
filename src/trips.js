1360968ef5c91ce4a6414b16356cdedc5aa19882
Michael Walker <Michael.e.walker.87@gmail.com>
2020-09-17 17:04:59 -0600

    Add the base tracked info for the trips

--- src/trips.js
+++ src/trips.js
@@ -1,6 +1,12 @@
 class Trips {
-  constructor(){
-
+  constructor(trip){
+    this.id = trip.id;  // number
+    this.userID = trip.userID;  // number
+    this.travelers = trip.travelers;  // number
+    this.date = trip.date;  // string
+    this.duration = trip.duration; // number
+    this.status = trip.status; // string
+    this.suggestedActivities = trip.suggestedActivities ; // array
   }
 }
 module.exports = Trips;
