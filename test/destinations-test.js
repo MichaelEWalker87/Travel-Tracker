a68b5787ad5483528ae05fa6a83ee70c7c918f7d
Michael Walker <Michael.e.walker.87@gmail.com>
2020-09-17 17:51:52 -0600

    Setup base structure for the destionation test

---
+++ test/destinations-test.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+
+import Destinations from '../src/destinations';
+import destination from '../src/data/destinations-data';
+
+describe('Destinations', () => {
+
+  beforeEach(() => {
+
+  });
+
+
+});
