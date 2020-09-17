0a625629e4a96862f7f37471d96e8740bc652e13
Michael Walker <Michael.e.walker.87@gmail.com>
2020-09-17 16:34:12 -0600

    Outline the Traveler class

--- src/travelers.js
+++ src/travelers.js
@@ -1,6 +1,22 @@
 class Travelers {
   constructor(traveler) {
-    
+    this.id =  traveler.id; //(number)
+    this.travelerType = traveler.travelerType; //(string)
+    this.userName = traveler.userName; // (string)
+    this.password = traveler.password; // (string)
+    this.past =  traveler.past; //  ([] of {})
+    this.present =  traveler.present; // ([] of {})
+    this.upcoming =  traveler.upcoming; // ([] of {})
+    this.pending =  traveler.pending; // ([] of {})
+    this.travelersTotal =  traveler.travelersTotal; //(number)
+  }
+
+  passwordUpdate() {
+
+  }
+
+  userNameUpdate() {
+
   }
 }
 module.exports = Travelers;
