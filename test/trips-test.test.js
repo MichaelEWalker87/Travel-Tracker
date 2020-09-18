86844edf597f17765759750f3e8ccb998569a0bb
Michael Walker <Michael.e.walker.87@gmail.com>
2020-09-18 08:43:06 -0600

    Test the properties of the class to verify accessing the right information

--- test/trips-test.test.js
+++ test/trips-test.test.js
@@ -19,4 +19,38 @@ describe('Trips', () => {
     expect(trip).to.be.an.instanceOf(Trips);
   });
 
+  describe('Properties of Trips', () => {
+
+    it("should be able to store a trip id", () => {
+      expect(trip.id).to.be.equal(1);
+    });
+
+    it("should be able to store another trip id", () => {
+      expect(trip2.id).to.be.equal(3);
+    });
+
+    it("should be able to store a user id", () => {
+      expect(trip.userID).to.be.equal(44);
+    });
+
+    it("should be able to store number of travelers", () => {
+      expect(trip.travelers).to.be.equal(1);
+    });
+
+    it("should be able to store date of trip", () => {
+      expect(trip.date).to.be.equal("2019/09/16");
+    });
+
+    it("should be able to store the trip duration", () => {
+      expect(trip.duration).to.be.equal(8);
+    });
+
+    it("should be able to store the trip duration", () => {
+      expect(trip.status).to.be.equal("approved");
+    });
+
+    it("should be able to store the trip duration", () => {
+      expect(trip.suggestedActivities).to.deep.equal([]);
+    });
+  });
 });
