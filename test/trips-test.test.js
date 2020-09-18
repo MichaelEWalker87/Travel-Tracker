af1b831acfc3ee640d68a66f2982afbcda004ad0
Michael Walker <Michael.e.walker.87@gmail.com>
2020-09-17 21:04:36 -0600

    Add Trip base test file set up to check to set if it is a function

--- test/trips-test.test.js
+++ test/trips-test.test.js
@@ -1,13 +1,22 @@
 import { expect } from 'chai';
 
 import Trips from '../src/trips';
-import trip from '../src/data/trips-data';
+import tripsData from '../src/data/trips-data';
 
 describe('Trips', () => {
-
+  let trip, trip2, trip3
   beforeEach(() => {
-
+    trip = new Trips(tripsData[0]);
+    trip2 = new Trips(tripsData[1]);
+    trip3 = new Trips(tripsData[2]);
   });
 
+  it("should be a function", () => {
+    expect(Trips).to.be.a("function");
+  });
+
+  it("should be an instance of Trips", () => {
+    expect(trip).to.be.an.instanceOf(Trips);
+  });
 
 });
