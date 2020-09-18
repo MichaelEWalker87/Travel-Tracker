a973387d9a7aeb430e7000669aa1f8bc2d3c39a9
Michael Walker <Michael.e.walker.87@gmail.com>
2020-09-17 20:31:59 -0600

    Conncet the data file for testing

--- test/travelers-test.js
+++ test/travelers-test.js
@@ -4,10 +4,18 @@ import Travelers from '../src/travelers';
 import travelers from '../src/data/travelers-data';
 
 describe('Travelers', () => {
-
+  let traveler, traveler2
   beforeEach(() => {
-
+    traveler = new Travelers(travelers[1])
+    traveler2 = new Travelers(travelers[2])
   });
 
+  it("should be a function", () => {
+    expect(Travelers).to.be.a("function");
+  });
+
+  it("should be an instance of Travelers", () => {
+    expect(traveler).to.be.an.instanceOf(Travelers);
+  });
 
 });
