d00fbb2cfaa0ae8883e9962a57dfbc26542fc0a7
Michael Walker <Michael.e.walker.87@gmail.com>
2020-09-18 22:00:01 -0600

    Add the destination 49 to be tested

--- src/data/destinations-data.js
+++ src/data/destinations-data.js
@@ -1,28 +1,38 @@
 let destinationData = [
   {
-  "id": 22,
-  "destination": "Rome, Italy",
-  "estimatedLodgingCostPerDay": 90,
-  "estimatedFlightCostPerPerson": 650,
-  "image": "https://images.unsplash.com/photo-1515542622106-78bda8ba0e5b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-  "alt": "people standing inside a colosseum during the day"
+    "id": 22,
+    "destination": "Rome, Italy",
+    "estimatedLodgingCostPerDay": 90,
+    "estimatedFlightCostPerPerson": 650,
+    "image": "https://images.unsplash.com/photo-1515542622106-78bda8ba0e5b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    "alt": "people standing inside a colosseum during the day"
   },
-{
-"id": 25,
-"destination": "New York, New York",
-"estimatedLodgingCostPerDay": 175,
-"estimatedFlightCostPerPerson": 200,
-"image": "https://images.unsplash.com/photo-1496442226666-8d4d0e62e6e9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-"alt": "people crossing the street during the day surrounded by tall buildings and advertisements"
-},
-{
-"id": 33,
-"destination": "Brussels, Belgium",
-"estLodgingCostPerDay": 1000,
-"estFlightCostPerPerson": 110,
-"image": "https://images.unsplash.com/photo-1559113202-c916b8e44373?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-"alt": "brown concrete gate"
-},
+  {
+    "id": 25,
+    "destination": "New York, New York",
+    "estimatedLodgingCostPerDay": 175,
+    "estimatedFlightCostPerPerson": 200,
+    "image": "https://images.unsplash.com/photo-1496442226666-8d4d0e62e6e9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    "alt": "people crossing the street during the day surrounded by tall buildings and advertisements"
+  },
+  {
+    "id": 33,
+    "destination": "Brussels, Belgium",
+    "estLodgingCostPerDay": 1000,
+    "estFlightCostPerPerson": 110,
+    "image": "https://images.unsplash.com/photo-1559113202-c916b8e44373?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    "alt": "brown concrete gate"
+  },
+  {
+    "id": 1,
+    "userID": 44,
+    "destinationID": 49,
+    "travelers": 1,
+    "date": "2019/09/16",
+    "duration": 8,
+    "status": "approved",
+    "suggestedActivities": []
+  },
 ]
 
 export default destinationData;
