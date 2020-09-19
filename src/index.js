133490456f2db3a7e3671def0eb79815fae20325
Michael Walker <Michael.e.walker.87@gmail.com>
2020-09-19 15:48:27 -0600

    Add images to make icons

--- src/index.js
+++ src/index.js
@@ -5,6 +5,25 @@
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import './images/beer.png'
+import './images/hop.png'
+import './images/Desert.jpg'
+import './images/honghong.jpg'
+import './images/honghong.jpg'
+import './images/Moutaintrain.png'
+import './images/new-york-city.jpg'
+import './images/Nightmountian.jpg'
+import './images/Outdoor.jpg'
+import './images/paris-eiffel-tower.jpg'
+import './images/pyramid-egypt.jpg'
+import './images/Seattle.jpg'
+import './images/present.png'
+import './images/pending.png'
+import './images/past.png'
+import './images/order.png'
+import './images/home.png'
+import './images/future.png'
+import './images/future.png'
+
 
 console.log('This is the JavaScript entry file - your code begins here.');
