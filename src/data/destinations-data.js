4d34f12104a753b324382b4cc915b8a906cb2e90
Michael Walker <Michael.e.walker.87@gmail.com>
2020-09-19 11:22:08 -0600

    Fix to the data to add the right name and set for testing

--- src/data/destinations-data.js
+++ src/data/destinations-data.js
@@ -18,18 +18,34 @@ let destinationData = [
   {
     "id": 33,
     "destination": "Brussels, Belgium",
-    "estLodgingCostPerDay": 1000,
-    "estFlightCostPerPerson": 110,
+    "estimatedLodgingCostPerDay": 1000,
+    "estimatedFlightCostPerPerson": 110,
     "image": "https://images.unsplash.com/photo-1559113202-c916b8e44373?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
     "alt": "brown concrete gate"
   },
   {
-  "id": 49,
-  "destination": "Castries, St Lucia",
-  "estimatedLodgingCostPerDay": 650,
-  "estimatedFlightCostPerPerson": 90,
-  "image": "https://images.unsplash.com/photo-1524478075552-c2763ea171b8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1502&q=80",
-  "alt": "aerial photography of rocky mountain under cloudy sky"
+    "id": 49,
+    "destination": "Castries, St Lucia",
+    "estimatedLodgingCostPerDay": 650,
+    "estimatedFlightCostPerPerson": 90,
+    "image": "https://images.unsplash.com/photo-1524478075552-c2763ea171b8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1502&q=80",
+    "alt": "aerial photography of rocky mountain under cloudy sky"
+  },
+  {
+    "id": 46,
+    "destination": "Victoria, Seychelles",
+    "estimatedLodgingCostPerDay": 1100,
+    "estimatedFlightCostPerPerson": 100,
+    "image": "https://images.unsplash.com/photo-1539980307411-6820f89db71b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1374&q=80",
+    "alt": "birds eye photography of body of water"
+  },
+  {
+    "id": 14,
+    "destination": "Marrakesh, Morocco",
+    "estimatedLodgingCostPerDay": 70,
+    "estimatedFlightCostPerPerson": 830,
+    "image": "https://images.unsplash.com/photo-1517821362941-f7f753200fef?ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80",
+    "alt": "people buying oranges and other fruit from a street vendor"
   },
 ]
 
