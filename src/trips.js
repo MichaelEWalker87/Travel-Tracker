76abe661eb898012e9c5dc264232bc8489e29c4d
Michael Walker <Michael.e.walker.87@gmail.com>
2020-09-18 21:37:17 -0600

    Add the price set to zero

--- src/trips.js
+++ src/trips.js
@@ -4,18 +4,30 @@ class Trips {
   constructor(trip) {
     this.id = trip.id;  // number
     this.userID = trip.userID;  // number
+    
     this.travelers = trip.travelers;  // number
     this.date = trip.date;  // string
     this.duration = trip.duration; // number
     this.status = trip.status; // string
     this.suggestedActivities = trip.suggestedActivities ; // array
+    this.price = 0;
     this.endDate = moment(this.date, "YYYY/MM/DD")
                     .add(this.duration, 'days')
                     .format("YYYY/MM/DD");
   }
 
-  calculatePrice() {
-    
+  calculatePrice(destinationData) {
+    // let selected = destinationData.find((destination) => {
+    //   return destination.
+    // })
+    // let cost = (
+    //   (this.duration * destinationData.estimatedLodgingCostPerDay * this.travelers)
+    //   +
+    //   (this.travelers * destinationData.estimatedFlightCostPerPerson)
+    // )
+    // this.price = Math.round(((cost * 0.1) + cost) * 100)/100
+    //
+    // console.log(destinationData)
   }
 
   submitRequest() {
